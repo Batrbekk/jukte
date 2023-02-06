6cadfc7cae0a7603c5e371eb8d434dbd9925f851
b.kuandyk <b.kuandyk@mycar.kz>
2023-02-06 11:02:32 +0600

    fix: add purge in next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
 }
