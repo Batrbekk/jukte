ec5ddb51fec0d32cc2b3d53315c3e64ad40b7a3f
b.kuandyk <b.kuandyk@mycar.kz>
2023-02-06 09:29:29 +0600

    fix: fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx", "./public/**/*.html"],
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
